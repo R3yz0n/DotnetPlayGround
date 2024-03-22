2c48def015ddcad7bd843fc6b4713bfb1376e423
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-22 19:38:54 +0545

    Update program comments and modify variable assignment

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,10 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// I'm working on a single file as of now we'll proceed with multiple files later on.
+
 
 // string firstName = "              BOb         sagar ";
 string firstName = " Sagar     Reyzon   ";
 
-// The Trim() method removes all the whitespaces in the front as wellas back wee also have TrimStart() and TrimEnd() to remove whitespaces from the start and end respectively
+// The Trim() method removes all the whitespaces in the front as wellas back wee also have TrimStart() and TrimEnd()
+// to remove whitespaces from the start and end respectively
 Console.WriteLine($"{firstName} {firstName.Length}");
 
 
