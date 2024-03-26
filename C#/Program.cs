7855ac7929d91d82322a95b28a72a4a16d6a7eab
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-26 19:59:30 +0545

    Remove extra blank line and add comment about Trim() method

--- C#/Program.cs
+++ C#/Program.cs
@@ -7,6 +7,7 @@ string firstName = " Sagar     Reyzon   ";
 // The Trim() method removes all the whitespaces in the front as wellas back wee also have TrimStart() and TrimEnd()
 // to remove whitespaces from the start and end respectively
 
+
 Console.WriteLine($"{firstName} {firstName.Length}");
 
 
