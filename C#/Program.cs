fa89edb38b981020029c40746673a1ba17d11d7f
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-31 22:25:36 +0545

    Refactor code: Remove unnecessary comments and initialize objects

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,14 +1,7 @@
-﻿// I'm working on a single file as of now we'll proceed with multiple files later on.
+﻿
+// WorkingWithString ws = new WorkingWithString();
+// WorkingWithNumber wn = new WorkingWithNumber();
 
-
-// string firstName = "              BOb         sagar ";
-string firstName = " Sagar     Reyzon   ";
-
-// The Trim() method removes all the whitespaces in the front as wellas back wee also have TrimStart() and TrimEnd()
-// to remove whitespaces from the start and end respectively
-
-
-Console.WriteLine($"{firstName} {firstName.Length}");
-
+WorkingWithString ws = new WorkingWithString();
 
 
