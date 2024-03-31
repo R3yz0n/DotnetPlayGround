0d685c50a5ea67ada41c7f51b6daf705c3031c8a
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-31 22:25:40 +0545

    Add WorkingWithNumber class with number manipulation logic

---
+++ C#/bcs/WorkingWithNumber.cs
@@ -0,0 +1,27 @@
+public class WorkingWithNumber
+{
+
+    public WorkingWithNumber()
+    {
+
+        short firstNumber = 5000;
+        short secondNumber = 5000;
+        short result = (short)(firstNumber + secondNumber);
+        // Console.WriteLine(typeof(short));
+        Console.WriteLine(checked(firstNumber + secondNumber));
+        // Console.WriteLine($"{firstNumber} {secondNumber}");
+
+        // int guessTheNumber = Convert.ToInt32(Console.ReadLine());
+        // decimal decinmalValue = 5.5M;
+
+        // Console.WriteLine(decinmalValue);
+        Console.WriteLine("hello");
+        double n = Convert.ToDouble("222.22");
+        Console.WriteLine(n);
+        System.Console.WriteLine("Hello World");
+        // System.Console.WriteLine(typeof(int));
+
+
+
+    }
+}
