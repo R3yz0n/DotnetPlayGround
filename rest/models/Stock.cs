576d05f9276eb95bbdacfe10ccf913d26a724317
R3yz0n <reyzon.thapa2020@gmail.com>
2024-04-04 19:54:12 +0545

    Add properties for CompanyName and Purchase in Stock.cs

--- rest/models/Stock.cs
+++ rest/models/Stock.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api.models
 {
@@ -9,11 +10,20 @@ namespace api.models
             get; set;
         }
         public string Symbol { get; set; } = string.Empty;
-        public Stock()
-        {
-            System.Console.WriteLine(string.Empty);
+        public string CompanyName { get; set; } = string.Empty;
+        // [Column(TypeName = "decimal(18, 2)")]
 
-        }
+        public decimal Purchase { get; set; }
+        // [Column(TypeName = "decimal(18, 2)")]
+
+
+
+        // public Stock()
+        // {
+        //     System.Console.WriteLine(string.Empty);
+
+        // }
 
     }
+
 }
