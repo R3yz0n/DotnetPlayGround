af91abe73cb7b80c3abb46e56f4f2b426b50a31c
R3yz0n <reyzon.thapa2020@gmail.com>
2024-04-06 14:03:44 +0545

    Add SmallMemoryManagement class for managing small memory in C#

---
+++ C#/bcs/SmallMemoryManagement.cs
@@ -0,0 +1,11 @@
+public class SmallMemoryManagement
+{
+    public SmallMemoryManagement()
+    {
+
+        //Primitive Data Type are stored in Stack and objects are stored in Heap 
+        //But there is a catch ????????????????
+
+    }
+
+}
