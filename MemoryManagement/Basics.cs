ef69da39425e0602af194f02f32d92fa48bd9992
R3yz0n <reyzon.thapa2020@gmail.com>
2024-04-06 14:12:11 +0545

    Add MemoryManagement/Basics.cs for basic memory management concepts

---
+++ MemoryManagement/Basics.cs
@@ -0,0 +1,24 @@
+
+public class Basics
+{
+    public Basics()
+    {
+        //Link to clear explanation
+        // https://www.youtube.com/watch?v=7O4JPdKjc30
+        //Primitive Data Type (value type) are stored in Stack and objects (refernce type) are stored in Heap 
+
+
+        //But there is a catch ????????????????
+
+
+
+    }
+
+    class Test
+    {
+
+        public string Name = string.Empty;
+        public int Age;
+    }
+
+}
