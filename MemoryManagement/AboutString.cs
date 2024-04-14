53dfbacb97c2ae0d0ed1d760d71b30eef4f5b1a4
R3yz0n <reyzon.thapa2020@gmail.com>
2024-04-14 15:12:02 +0545

    Add MemoryManagement/AboutString.cs for demonstrating string manipulation

---
+++ MemoryManagement/AboutString.cs
@@ -0,0 +1,14 @@
+using System;
+class AboutStrings
+{
+    public AboutStrings()
+
+    {
+        string x = "sagar";
+        int y = 100;
+
+        System.Console.WriteLine(x1);
+        x = Convert.ToString(y);
+        System.Console.WriteLine(x);
+    }
+}
