a8a57c6c6f8853d022db1929110837f01443961d
R3yz0n <reyzon.thapa2020@gmail.com>
2024-04-14 15:12:16 +0545

    Add MemoryManagement/AboutStrings.cs for demonstrating string manipulation and memory management

--- MemoryManagement/Program.cs
+++ MemoryManagement/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+// Stack Heap in c# && Boxing Unboxing && Value Type Reference Type
+
+// https://www.youtube.com/watch?v=7O4JPdKjc30
+
+System.Console.WriteLine("Stack Heap in c# && Boxing Unboxing && Value Type Reference Type");
+
+// WhatGoesWhere whatGoesWhere = new WhatGoesWhere();
+
+// Types t = new Types();
+// BoxingUnboxing b = new BoxingUnboxing();
+AboutStrings h = new AboutStrings();
