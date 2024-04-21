f02feb57dff446d32d335be13ba4bb6ea6a3f0c1
R3yz0n <reyzon.thapa2020@gmail.com>
2024-04-21 22:24:39 +0545

    Add LINQ functionality to Program.cs

--- C#/Program.cs
+++ C#/Program.cs
@@ -2,4 +2,6 @@
 // WorkingWithString ws = new WorkingWithString();
 // WorkingWithNumber wn = new WorkingWithNumber();
 // WorkingWithConditions wc = new WorkingWithConditions();
-WorkingWithT wt = new WorkingWithT();
+// WorkingWithT wt = new WorkingWithT();
+// SortingSearching ss = new SortingSearching();
+LINQ l = new LINQ();
